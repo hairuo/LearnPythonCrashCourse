76d39307dc82a20aae335e8cbe2768b62a174116
hairuo <hairuo@users.noreply.github.com>
2017-06-19 22:39:20 +0800

    update before section 14.3.7

---
+++ alien_invasion/scoreboard.py
@@ -0,0 +1,55 @@
+import pygame.font
+
+
+
+
+
+class Scoreboard():
+	"""A class to report scoring information."""
+	
+	def __init__(self, ai_settings, screen, stats):
+		"""Initialize scorekeeping attributes."""
+		self.screen = screen
+		self.screen_rect = screen.get_rect()
+		self.ai_settings = ai_settings
+		self.stats = stats
+		
+		# Font settings for scoring information.
+		self.text_color = (30, 30, 30)
+		self.font = pygame.font.SysFont(None, 48)
+		
+		# Prepare the initial score image.
+		self.prep_score()
+		#~ self.prep_high_score()
+
+	def prep_score(self):
+		"""Turn the score into a redered image."""
+		rounded_score = int(round(self.stats.score, -1))
+		score_str = "{:,}".format(rounded_score)
+		self.score_image = self.font.render(score_str, True, self.text_color,
+			self.ai_settings.bg_color)
+			
+		# Display the score at the top right of the screen.
+		self.score_rect = self.score_image.get_rect()
+		self.score_rect.right = self.screen_rect.right - 20
+		self.score_rect.top = 20
+		
+	#~ def prep_high_score(self):
+		#~ """Turn the high score into a rendered image."""
+		#~ high_score = int(round(self.stats.high_score, -1))
+		#~ high_score_str = "{:,}".format(high_score)
+		#~ self.high_score.image = self.font.render(high_score_str, True,
+			#~ self.text_color, self.ai_settings.bg_color)
+			
+		#~ # Center the high score at the top of the screen.
+		#~ self.high_score_rect = self.high_score_image.get_rec()
+		#~ self.high_score_rect.centerx = self.screen_rect.centerx
+		#~ self.high_score_rect.top = self.score_rect.top
+	
+
+
+	def show_score(self):
+		"""Draw score to the screen."""
+		self.screen.blit(self.score_image, self.score_rect)
+		#~ self.screen.blit(self.high_score_image, self.high_score_rect)
+		
