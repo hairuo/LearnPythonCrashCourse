54536f969a6b12cddc668ed7c4690bff4440f427
hairuo <hairuo@users.noreply.github.com>
2017-06-20 11:22:17 +0800

    correct some erros

--- alien_invasion/scoreboard.py
+++ alien_invasion/scoreboard.py
@@ -38,11 +38,11 @@ class Scoreboard():
 		"""Turn the high score into a rendered image."""
 		high_score = int(round(self.stats.high_score, -1))
 		high_score_str = "{:,}".format(high_score)
-		self.high_score.image = self.font.render(high_score_str, True,
+		self.high_score_image = self.font.render(high_score_str, True,
 			self.text_color, self.ai_settings.bg_color)
 			
 		# Center the high score at the top of the screen.
-		self.high_score_rect = self.high_score_image.get_rec()
+		self.high_score_rect = self.high_score_image.get_rect()
 		self.high_score_rect.centerx = self.screen_rect.centerx
 		self.high_score_rect.top = self.score_rect.top
 	
