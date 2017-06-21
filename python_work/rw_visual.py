c22da9c08e4bca33e0d2cfa66a1f6a80277df89b
hairuo <hairuo@users.noreply.github.com>
2017-06-21 16:01:48 +0800

    Chapter 15 updated

--- python_work/rw_visual.py
+++ python_work/rw_visual.py
@@ -2,8 +2,31 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
-# Make a random walk, and plot the points.
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+# Keep making new walks, as long as the program is active.
+
+while True:
+	# Make a random walk, and plot the points.
+	rw = RandomWalk(5000)
+	rw.fill_walk()
+	
+	# Set the size of the plotting window.
+	plt.figure(dpi=100, figsize=(16,8))
+	
+	point_numbers = list(range(rw.num_points))
+	plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
+		edgecolor='none', s=1)
+
+	# Emphasize the first and last points.
+	plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+	plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
+		s=100)
+		
+	# Remove the axes.
+	plt.axes().get_xaxis().set_visible(False)
+	plt.axes().get_yaxis().set_visible(False)
+	
+	plt.show()
+	
+	keep_running = input("Make another walk? (y/n): ")
+	if keep_running == 'n':
+		break
