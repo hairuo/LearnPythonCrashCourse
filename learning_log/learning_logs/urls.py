1c189672bab9ddd98998528528a5762adbdca354
hairuo <hairuo@users.noreply.github.com>
2017-06-23 21:52:15 +0800

    Finished Chapter 18, Getting Started with Django

---
+++ learning_log/learning_logs/urls.py
@@ -0,0 +1,15 @@
+"""Defines URL patterns for learing_logs"""
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	# Home page
+	url(r'^$', views.index, name='index'),
+	
+	# Show all topics.
+	url(r'^topics/$', views.topics, name='topics'),
+	# Detail page for a single topic
+	url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+]	
---
+++ learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!f:\download\programming\python\learnpythoncrashcourse\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
