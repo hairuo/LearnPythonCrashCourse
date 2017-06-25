1e30b05d2de1cc5f8b3f63ebabe27e0ed38d6a01
hairuo <hairuo@users.noreply.github.com>
2017-06-25 11:40:41 +0800

    Finished Section 19.2

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My apps
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^users/', include('users.urls', namespace='users')),
     url(r'', include('learning_logs.urls', namespace='learning_logs')),
 ]
